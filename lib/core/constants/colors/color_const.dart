b143840424db7cbe9c350a80dae3432180fd46b9
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-06-21 11:16:29 +0500

    font services and extensions were added

---
+++ lib/core/constants/colors/color_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ColorConst {
+  static const Color kPrimaryColor = Color(0XFF121212);
+  static const Color buttonColor = Color(0XFFD50109);
+  static const Color buttonBlueColor = Color(0XFF4C8BF5);
+  static const Color whiteColor = Colors.white;
+}
